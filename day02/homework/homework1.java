ce9d4a3bc6a8ecd9aff3ee456057eefebb98f7ad
疾风剑豪 <115929650+sengezuishuai@users.noreply.github.com>
2023-02-28 21:29:17 +0800

    Add files via upload

---
+++ day02/homework/homework1.java
@@ -0,0 +1,16 @@
+public class homework1{
+    public static void main(String[] args){
+        String n1 = "皮卡丘";
+        String n2 = "大冒险";
+        String n3 = n1 + n2;
+        char n4 = '男';
+        char n5 = '女';
+        char n6 = (char)(n4 + n5);
+        int n7 = 56;
+        int n8 = 47;
+        int n9 = n7 + n8;
+        System.out.println(n3);
+        System.out.println(n4 + n5);
+        System.out.println(n9);
+    }
+}
---
+++ day02/homework/homework2.java
@@ -0,0 +1,11 @@
+public class homework2 {
+    public static void main(String[] args){
+    String name = "jake";
+    Byte age = 38;
+    char sex = '女';
+    double num = 95.67;
+    String hobby = "打篮球";
+    System.out.println("姓名\t年龄\t成绩\t性别\t爱好\n"
+    + name + '\t' + age +'\t' + num + '\t' + sex + '\t' + hobby);
+    }
+}
