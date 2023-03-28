b5b542ca545d13307d5817fd1fca448571ba4df1
疾风剑豪 <115929650+sengezuishuai@users.noreply.github.com>
2023-03-28 17:01:57 +0800

    Add files via upload

---
+++ practice_day23/src/com/senge/final_/final01.java
@@ -0,0 +1,13 @@
+package com.senge.final_;
+
+public class final01 {
+    public static void main(String[] args) {
+
+    }
+
+    public final01() {
+    }
+}
+
+
+
---
+++ practice_day23/src/com/senge/static_/childGame.java
@@ -0,0 +1,9 @@
+package com.senge.static_;
+
+public class childGame {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
