6751bc8bfb00d57eed120baecf21bb918cdbd6a9
疾风剑豪 <115929650+sengezuishuai@users.noreply.github.com>
2023-03-29 14:03:02 +0800

    Add files via upload

---
+++ practice_day24/src/com/senge/abstract_/interfaces.java
@@ -0,0 +1,23 @@
+package com.senge.abstract_;
+
+public class interfaces {
+
+}
+
+interface IA{
+    void say();
+    void hi();
+}
+
+class cat implements IA{
+
+    @Override
+    public void say() {
+
+    }
+
+    @Override
+    public void hi() {
+
+    }
+}
